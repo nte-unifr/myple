f598b56bd7fc87338936ac98f466d72f0840d1ec
Alrick <alrick.deillon@unifr.ch>
2017-03-22 17:00:38 +0100

    add arraycollection for relationships

--- src/AppBundle/Entity/ToolFamily.php
+++ src/AppBundle/Entity/ToolFamily.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * ToolFamily
