5f40f6dc67a2f950bb088d0659f5f76b44b6b18e
Alrick <alrick.deillon@unifr.ch>
2017-05-16 14:07:13 +0200

    add recap pdf to task

--- src/AppBundle/Entity/Task.php
+++ src/AppBundle/Entity/Task.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Doctrine\Common\Collections\ArrayCollection;
 
 /**
@@ -10,6 +12,7 @@ use Doctrine\Common\Collections\ArrayCollection;
  *
  * @ORM\Table(name="task")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\TaskRepository")
+ * @Vich\Uploadable
  */
 class Task
 {
@@ -61,6 +64,25 @@ class Task
      * @ORM\OneToMany(targetEntity="Activity", mappedBy="task")
      */
     private $activities;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string
+     */
+    private $recap;
+
+    /**
+     * @Vich\UploadableField(mapping="task_recap", fileNameProperty="recap")
+     * @var File
+     */
+    private $recapFile;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     *
+     * @var \DateTime
+     */
+    private $updatedAt;
 
 
     /**
@@ -232,5 +254,57 @@ class Task
     public function getHelp()
     {
         return $this->help;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $file
+     *
+     * @return Task
+     */
+    public function setRecapFile(File $file = null)
+    {
+        $this->recapFile = $file;
+
+        if ($file) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getRecapFile()
+    {
+        return $this->recapFile;
+    }
+
+    /**
+     * @param string $recap
+     *
+     * @return Task
+     */
+    public function setRecap($recap)
+    {
+        $this->recap = $recap;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRecap()
+    {
+        return $this->recap;
     }
 }
