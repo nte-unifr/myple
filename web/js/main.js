833a13fe7eb71c5b049e3723cc16513da4bf98bf
Alrick Deillon <deilloal@ditntemac03.unifr.ch>
2017-07-27 15:44:20 +0200

    allow to hide intro-video and auto-hide for small resolutions

---
+++ web/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // hide video if asked
+    if (store.get('hide-video')) {
+        $('#video-panel').hide()
+    }
+    // trigger click to hide video
+    $('#video-panel button').click(function () {
+        $('#video-panel').hide()
+        store.set('hide-video', true)
+    })
+})
