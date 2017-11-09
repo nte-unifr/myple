f3cb9efc9ffafe9b594ce2c50cddf1c128fc9997
Alrick <alrick.deillon@unifr.ch>
2017-11-09 17:39:02 +0100

    starting tools imp

---
+++ src/AppBundle/Controller/ToolController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ToolController extends Controller
+{
+    /**
+     * @Route("/tools", name="tools_index")
+     */
+    public function indexAction()
+    {
+        $repository = $this->getDoctrine()->getRepository("AppBundle:Tool");
+        $tools = $repository->findAll();
+
+        return $this->render('tools/index.html.twig', array(
+            'tools' => $tools
+        ));
+    }
+}
