679b962e724675f95a022e959da538390dba1664
Alrick <alrick.deillon@unifr.ch>
2018-07-23 15:08:06 +0200

    i18n for most of public views

--- src/AppBundle/Controller/ToolController.php
+++ src/AppBundle/Controller/ToolController.php
@@ -4,19 +4,22 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ToolController extends Controller
 {
     /**
      * @Route("/{_locale}/tools", name="tools_index")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $repository = $this->getDoctrine()->getRepository("AppBundle:Tool");
         $tools = $repository->findAll();
 
+        $locale = ucfirst($request->getLocale());
+        $sort = 'name'.$locale;
         $familiyRepository = $this->getDoctrine()->getRepository("AppBundle:ToolFamily");
-        $families = $familiyRepository->findBy(array(), array('nameFr' => 'ASC'));
+        $families = $familiyRepository->findBy(array(), array($sort => 'ASC'));
 
         return $this->render('tools/index.html.twig', array(
             'tools' => $tools,
