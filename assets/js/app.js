c849759821177bfdd68b2c5e505cc812c3b2bc80
Alrick <alrick.deillon@unifr.ch>
2019-06-17 15:35:24 +0200

    add filtering in the url

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,8 @@ $(document).ready(function () {
   }
 })
 
+// < Custom functions
+// 
 function videoStore() {
   // hide video if asked
   if (!store.get('hide-video')) {
@@ -27,7 +29,11 @@ function videoStore() {
     store.set('hide-video', true)
   })
 }
+//
+// Custom functions >
 
+// < Bootstrap functions
+// 
 function initBsElements() {
   // resources popover
   $('.popover-toggler').popover({
@@ -51,11 +57,21 @@ function initBsElements() {
   // tooltips activation
   $('[data-toggle="tooltip"]').tooltip()
 }
+//
+// Bootstrap functions >
 
+// < Mixitup functions
+// 
 function initMixitup() {
+  var targetSelector = '.mix'
   var mixer = mixitup('#tools', {
     load: {
-      sort: 'order:asc'
+      sort: 'order:asc',
+      filter: function() {
+        var hash = window.location.hash.replace(/^#/g, '')
+        var selector = hash ? '.' + hash : targetSelector
+        return selector
+      }()
     },
     selectors: {
       control: '[data-mixitup-control]'
@@ -63,7 +79,13 @@ function initMixitup() {
     callbacks: {
       onMixStart: function() {
         $('.tools-popover').popover('hide')
+      },
+      onMixEnd: function(state) {
+        var newHash = '#' + state.activeFilter.selector.replace(/^\./g, '')
+        history.replaceState(undefined, undefined, newHash)
       }
     }
   })
 }
+//
+// Mixitup functions >
