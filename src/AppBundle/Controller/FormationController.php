ce73b133cba0bff2fdbdd89e238295a392ea7ccf
Alrick Deillon <alrick.deillon@unifr.ch>
2022-03-17 09:31:54 +0100

    Sort formations by title ASC

--- src/AppBundle/Controller/FormationController.php
+++ src/AppBundle/Controller/FormationController.php
@@ -15,12 +15,11 @@ class FormationController extends Controller
     public function indexAction(Request $request, NavService $navService)
     {
         $repository = $this->getDoctrine()->getRepository("AppBundle:Formation");
-        $formations = $repository->findAll();
+        $locale = ucfirst($request->getLocale());
+        $sort = 'title'.$locale;
+        $formations = $repository->findBy(array(), array($sort => 'ASC'));
 
-        // $locale = ucfirst($request->getLocale());
-        // $sort = 'name'.$locale;
-        // $familiyRepository = $this->getDoctrine()->getRepository("AppBundle:ToolFamily");
-        // $families = $familiyRepository->findBy(array(), array($sort => 'ASC'));
+        //$formations = $repository->findAll();
 
         return $this->render('formations/index.html.twig', array(
             'formations' => $formations,
